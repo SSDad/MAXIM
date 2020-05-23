334e9ec2024c3ba3cae0f14c62a42d437df1c2b7
Zhen Ji <jizhen04@gmail.com>
2020-05-23 18:26:56 -0500

    freehand, but only straight lines...

---
+++ gui_MAXIM/Callback_Pushbutton_SnakePanel_FreeHand.m
@@ -0,0 +1,26 @@
+function Callback_Pushbutton_SnakePanel_FreeHand(src, evnt)
+
+hFig = ancestor(src, 'Figure');
+data = guidata(hFig);
+
+hSlider = data.Panel.View.Comp.hSlider.Slice;
+data.FreeHand.iSlice = round(hSlider.Value);
+
+hA = data.Panel.View.Comp.hAxis.Image;
+uistack(hA, 'top');
+
+axis(hA);
+L = images.roi.AssistedFreehand(hA,...
+    'Image', data.Panel.View.Comp.hPlotObj.Image, ...
+    'Closed', 0);
+% L = images.roi.AssistedFreehand('Image', data.Panel.View.Comp.hPlotObj.Image, ...
+%     'Closed', 0);
+draw(L);
+
+data.FreeHand.L = L;
+data.FreeHand.Done = true;
+
+data.Panel.Snake.Comp.Panel.FreeHand.Togglebutton.Slither.Enable = 'on';
+
+%% save
+guidata(hFig, data);                
